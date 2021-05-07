7f860c8ad7fab94d0ab19821fa8c06e9e47d188f
ronger <ronger-x@outlook.com>
2021-05-07 08:38:24 +0800

    :poop: 公告组件

--- components/announcement/list.vue
+++ components/announcement/list.vue
@@ -3,12 +3,18 @@
     <el-col :xs="24" :sm="24" :xl="24" style="margin: 0 auto;">
       <el-carousel :interval="5000" type="card" height="256px">
         <el-carousel-item v-for="article in announcements.articles" :key="article.idArticle" style="padding-bottom: 1rem;">
-          <el-card>
+          <el-card style="height: 250px;">
             <div class="card-body d-flex flex-column">
               <el-link rel="nofollow" @click="onRouter('article',article.articleLink)" :underline="false"
-                       style="margin-bottom: .5rem;">
+                       style="margin-bottom: .7rem;">
                 <h4>
                   {{ article.articleTitle }}
+                  <el-tag
+                  style="margin-left: 0.5rem;"
+                  size="mini"
+                  effect="plain">
+                  # 公告
+                </el-tag>
                 </h4>
               </el-link>
               <div class="text-muted article-summary-md">{{ article.articlePreviewContent }}</div>
