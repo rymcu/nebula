ee69e6bd0faf14600681913a41c8ec26fe4d6bc3
祝梦园 <zhumengyua@qq.com>
2024-01-03 18:26:23 +0800

    简单修复专题管理页面列表

--- components/archive/list.vue
+++ components/archive/list.vue
@@ -12,7 +12,7 @@
                     d="M12 22c3.859 0 7-3.141 7-7s-3.141-7-7-7c-3.86 0-7 3.141-7 7s3.14 7 7 7zm0-12c2.757 0 5 2.243 5 5s-2.243 5-5 5-5-2.243-5-5 2.243-5 5-5zm-1-8H7v5.518a8.957 8.957 0 0 1 4-1.459V2zm6 0h-4v4.059a8.957 8.957 0 0 1 4 1.459V2z"></path><path
                     d="m10.019 15.811-.468 2.726L12 17.25l2.449 1.287-.468-2.726 1.982-1.932-2.738-.398L12 11l-1.225 2.481-2.738.398z"></path></svg>
                 </span>
-              <span style="font-size: 20px" v-html="article.articleTitle"> </span>
+              <span class="articleTitle" v-html="article.articleTitle"> </span>
               <el-tag size="mini" v-for="tag in article.tags" :key="tag.idTag" style="margin-right: 10px"> {{ tag.tagTitle }}</el-tag>
 
               <div class="text-muted article-summary-md" v-html="article.articlePreviewContent"></div>
@@ -81,5 +81,9 @@ export default {
 </script>
 
 <style scoped>
+.articleTitle {
+  font-size: 20px;
+  font-weight: 600;
+}
 
 </style>
--- components/layouts/pc/header.vue
+++ components/layouts/pc/header.vue
@@ -250,6 +250,7 @@ export default {
       }
     },
     getUnreadNotifications() {
+      console.log('我执行了')
       let _ts = this;
       if (_ts.user) {
         _ts.$axios.$get('/api/notification/unread').then(function (res) {
